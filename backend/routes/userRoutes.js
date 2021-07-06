46f715294d4ab74e43e3727429d1a9cef41780eb
pzou15 <84814375+pzou15@users.noreply.github.com>
2021-07-06 13:31:34 -0400

    Update userRoutes.js
    Initial commit for password recovery API

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,16 +1,37 @@
 const express = require('express');
 const userModel = require('../models/user');
 const app = express();
+const { Keccak } = require('sha3');
 
 app.post('/create-user', async (request, response) => {
-    const user = new userModel(request.body);
+  const user = new userModel(request.body);
 
-    try {
-        await user.save();
-        response.send(user);
-    } catch (error) {
-        response.status(500).send(error);
-    }
+  try {
+    await user.save();
+    response.send(user);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+app.post('/update-password', async (request, response) => {
+  // incoming: ID, password, retyped password
+  // outgoing: updated password
+  
+  const { ID, Password1, Password2 } = request.body;
+  const hash = new Keccak(256);
+  
+  if (Password1 == Password2) {
+    hash.update(Password1);
+    await userModel.findByIdAndUpdate({_id : ID}, { password : hash.digest('hex') },
+      function (err, result) {
+        if (err) response.send(err);
+        else response.send('Updated Password');
+    });
+  }
+  else {
+    response.send('Passwords do not match.');
+  }
 });
 
 
