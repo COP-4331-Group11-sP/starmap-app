1dad71e8d18b9395fb94cb6f5cf9ed5d8f81d278
pwhph1 <68026625+pwhph1@users.noreply.github.com>
2021-07-27 00:25:55 -0400

    Update faveRoute.js

--- backend/routes/faveRoute.js
+++ backend/routes/faveRoute.js
@@ -5,7 +5,17 @@ const app = express();
 
 app.post('/create-favorites', async (request, response) => {
     const favorite = new faveModel(request.body);
-
+    
+    let duplicates = await faveModel.find({starId: request.body.starId, userId: request.body.userId});
+    duplicates = duplicates.length;
+    
+    // prevents duplicate starId
+    if (duplicates > 0)
+    {
+        response.status(401).send('duplicate found');
+        return;
+    };
+    
     try {
         await favorite.save();
         response.send(favorite);
