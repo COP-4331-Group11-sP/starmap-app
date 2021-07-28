b9c495c082ee58866d1bff715716b1298e53ea53
Kaptinketchup <51547523+Kaptinketchup@users.noreply.github.com>
2021-07-27 22:18:01 -0400

    Update fave_controller.js

--- backend/controllers/fave_controller.js
+++ backend/controllers/fave_controller.js
@@ -1,5 +1,6 @@
 const Fave = require('../models/fave.js');
 
+//UPDATES FAVORITE STAR
 exports.updateFave = async (req, res) => {
   const { docID, Notes } = req.body;
 
@@ -12,3 +13,64 @@ exports.updateFave = async (req, res) => {
     return res.status(500).send(err);
   }
 }
+
+//ADDS FAVORITE STAR
+exports.addFave = async (req, res) => {
+  const { displayId, starId, userId, notes } = req.body;
+
+  let duplicates = await Fave.find({starId, userId})
+  duplicates = duplicates.length;
+
+  // prevents duplicate starId
+  if (duplicates > 0)
+  {
+      res.status(401).send(
+        {
+          message: 'duplicate found'
+        });
+      return;
+  };
+  
+  const fave = new Fave({
+      displayId: displayId,
+      starId: starId,
+      userId: userId,
+      notes: notes
+  })
+
+  try {
+    const newFave = await fave.save()
+    res.status(200).json(newFave)
+  } catch (err) {
+      response.status(500).send(error);
+  }
+}
+
+// Delete Favorite Star
+exports.deleteFave = async (req, res) => {
+  try {   
+    const favorite = await Fave.findByIdAndDelete(request.body);
+    if (!favorite) response.status(404).send("No item found");
+    response.status(200).send();
+  } catch (error) {
+    response.status(500).send(error);
+  }
+}
+
+// Search Favorite Star
+exports.searchFave = async (req, res) => {
+  const { displayId, userId } = req.body;
+  const search = displayId
+  const regex = new RegExp(search, 'i') // i for case insensitive
+  
+  let Favorites = await Fave.find({displayId: {$regex: regex}, userId})
+  if (!Favorites) response.status(404).send("No item found");
+
+  try {
+    res.status(200).json(Favorites)
+  } catch (err) {
+      response.status(500).send(error);
+  }
+  }
+
+
